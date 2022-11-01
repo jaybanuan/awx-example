384efa06be68ad80d610db1a0b2cd3fdb9fd72ba
jaybanuan <40902617+jaybanuan@users.noreply.github.com>
2022-11-02 01:34:01 +0900

    update

--- deploy-awx-example.sh
+++ deploy-awx-example.sh
@@ -1,44 +1,40 @@
 #!/bin/bash
 
-ORGANIZATION_ID=$(awx organization get -f jq --filter '.id' Default)
-
 awx project create \
     --name awx-example-project \
-    --organization "${ORGANIZATION_ID}" \
+    --organization Default \
     --scm_type git \
     --scm_url https://github.com/jaybanuan/awx-example.git \
-    --wait \
-    --monitor
+    --wait
 
 awx inventory create \
     --name web-server-inventory \
-    --organization "${ORGANIZATION_ID}"
+    --organization Default
 
 awx inventory_source create \
     --name web-server-inventory-source \
-    --inventory $(awx inventory get -f jq --filter '.id' web-server-inventory) \
+    --inventory web-server-inventory \
     --source scm \
-    --source_project $(awx project get -f jq --filter '.id' awx-example-project) \
+    --source_project awx-example-project \
     --source_path inventories/web-server.yml
 
 awx inventory_source update \
     --wait \
-    --monitor \
     web-server-inventory-source
 
 awx credential create \
     --name web-server-credential \
+    --organization Default \
     --credential_type Machine \
-    --organization "${ORGANIZATION_ID}" \
     --inputs '{"password": "root", "username": "root"}'
 
 awx job_template create \
     --name web-server-job-template \
     --job_type run \
-    --inventory $(awx inventory get -f jq --filter '.id' web-server-inventory) \
-    --project $(awx project get -f jq --filter '.id' awx-example-project) \
+    --inventory web-server-inventory \
+    --project awx-example-project \
     --playbook playbooks/web_server.yml
 
 awx job_template associate \
     --credential web-server-credential \
-    $(awx job_template get -f jq --filter '.id' web-server-job-template)
+    web-server-job-template
